7e48ef1dac0a9b9f5508bcfd77e13735bb8a6c0e
earlbert <earlbert01@gmail.com>
2025-05-26 22:47:07 +0800

    added HeroImages test case

--- src/main/java/odotatesting/constants/HeroesPageLocators.java
+++ src/main/java/odotatesting/constants/HeroesPageLocators.java
@@ -6,4 +6,5 @@ public class HeroesPageLocators {
     public static final String PUBLIC_TAB = "//span[normalize-space()='Public']";
     public static final String TURBO_TAB = "//span[normalize-space()='Turbo']";
     public static final String MATCHES_COUNT_IN_SEVEN_DAYS = "//span[@class='subtitle']";
+    public static final String HERO_IMAGE = "//tbody//img[1]";
 }
