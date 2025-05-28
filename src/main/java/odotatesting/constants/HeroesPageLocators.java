a04f8d47e06c5b26129808d61732b7c9ecac8925
earlbert <earlbert01@gmail.com>
2025-05-28 23:26:36 +0800

    added new test case to check if there are null data in heroes table

--- src/main/java/odotatesting/constants/HeroesPageLocators.java
+++ src/main/java/odotatesting/constants/HeroesPageLocators.java
@@ -10,4 +10,12 @@ public class HeroesPageLocators {
     public static final String ROWS_LOCATOR = "xpath=/html//table[1]/tbody[1]/tr";
     public static final String HERO_NAME_LOCATOR_RELATIVE = "//td[1]//a";
     public static final String HERO_TABLE_ROWS_XPATH = "xpath=/html//table[1]/tbody[1]/tr";
+    public static final String PRO_PICK_PLUS_BAN_PERCENTAGE = "//table/tbody/tr/td[2]//span";
+    public static final String PRO_PICK_PLUS_BAN_COUNT = "//table/tbody/tr/td[2]//small";
+    public static final String PRO_PICK_PERCENTAGE = "//table/tbody/tr/td[3]//span";
+    public static final String PRO_PICK_COUNT = "//table/tbody/tr/td[3]//small";
+    public static final String PRO_BAN_PERCENTAGE = "//table/tbody/tr/td[4]//span";
+    public static final String PRO_BAN_COUNT = "//table/tbody/tr/td[4]//small";
+    public static final String PRO_WIN_PERCENTAGE = "//table/tbody/tr/td[5]//span";
+    public static final String PRO_WIN_COUNT = "//table/tbody/tr/td[5]//small";
 }
