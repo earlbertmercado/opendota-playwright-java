7972e729abcda377c03685fb53d64fd91cdd5153
earlbertmercado <earlbertmercado@rccl.com>
2025-06-04 15:56:45 +0800

    added way to handle small screen

---
+++ src/main/java/odotatesting/constants/BurgerMenuLocators.java
@@ -0,0 +1,15 @@
+package odotatesting.constants;
+
+public class BurgerMenuLocators {
+
+    public static final String MATCHES = "//html//ul[1]/a[1]/li/div/span";
+    public static final String HEROES = "//html//ul[1]/a[2]/li/div/span";
+    public static final String TEAMS = "//html//ul[1]/a[3]/li/div/span";
+    public static final String EXPLORER = "//html//ul[1]/a[4]/li/div/span";
+    public static final String COMBOS = "//html//ul[1]/a[5]/li/div/span";
+    public static final String DISTRIBUTIONS = "//html//ul[6]/a[1]/li/div/span";
+    public static final String RECORDS = "//html//ul[1]/a[7]/li/div/span";
+    public static final String SCENARIOS = "//html//ul[1]/a[8]/li/div/span";
+    public static final String API = "//html//ul[1]/a[9]/li/div/span";
+    public static final String SUBSCRIBE = "//html//ul[1]/a[10]/li/div/span";
+}
