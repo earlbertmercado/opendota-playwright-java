5e19ec56313354225f47372325ee8b39659d0a13
earlbert <earlbert01@gmail.com>
2025-06-07 13:38:53 +0800

    moved takeScreenshot from factory to utils package

--- src/main/java/odotatesting/factory/PlaywrightFactory.java
+++ src/main/java/odotatesting/factory/PlaywrightFactory.java
@@ -1,10 +1,5 @@
 package odotatesting.factory;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Properties;
 
@@ -13,7 +8,6 @@ import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.options.ScreenshotType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -96,48 +90,5 @@ public class PlaywrightFactory {
         // Remove the ThreadLocal value after use
         tlPage.remove();
         tlFactory.remove();
-    }
-
-    public String takeScreenshot(String scenarioName) {
-        Page currentPage = getPage();
-        if (currentPage == null) {
-            System.err.println("Page instance is null. Cannot take screenshot.");
-            return null;
-        }
-        String screenshotDir = System.getProperty("user.dir") + "/reports/screenshots/";
-        Path screenshotDirPath = Paths.get(screenshotDir);
-
-        try {
-            Files.createDirectories(screenshotDirPath); // Creates directories if they don't exist
-        } catch (Exception e) {
-            System.err.println("Failed to create screenshot directory." + e.getMessage());
-            return null;
-        }
-
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
-        String timestamp = now.format(formatter);
-
-        // Clean scenario name for filename safety
-        String cleanScenarioName = scenarioName.replaceAll("[^a-zA-Z0-9.-]", "_");
-        String filename = cleanScenarioName + "_" + timestamp + ".png";
-
-        Path fullPath = screenshotDirPath.resolve(filename);
-
-        try {
-            currentPage.screenshot(new Page.ScreenshotOptions()
-                    .setPath(fullPath)
-                    .setType(ScreenshotType.PNG)
-                    .setFullPage(true));
-
-            String relativePathForReport = "./screenshots/" + filename; // Path relative to the HTML report
-
-            System.out.println("Screenshot saved to: " + fullPath);
-            System.out.println("Path for report: " + relativePathForReport);
-            return relativePathForReport;
-        } catch (Exception e) {
-            System.err.println("Failed to take screenshot: " + e.getMessage());
-            return null;
-        }
     }
 }
